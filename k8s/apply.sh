730907f5d773574f93cb52fe683a700e81980b51
Mark Schenk <m.m.a.schenk@tudelft.nl>
2021-07-05 10:26:32 +0200

    No default for namespace in script!

---
+++ k8s/apply.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export NS=
+
+
+if [ "${NS}" == "" ]; then
+  echo "Set the NS environment variable to your desired namespace"
+  exit
+fi
+
+kubectl -n ${NS} apply -f datavolumes.yaml
+kubectl -n ${NS} apply -f config.yaml
+kubectl -n ${NS} apply -f database.yaml
